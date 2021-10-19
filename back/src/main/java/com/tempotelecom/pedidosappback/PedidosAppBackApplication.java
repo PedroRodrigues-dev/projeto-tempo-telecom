533f2f6293963c1c3449da3ccdb97f144152daa7
Pedro Rodrigues <pedroras004@gmail.com>
2021-10-19 17:15:05 -0300

    Adiciona base Back Springboot

---
+++ back/src/main/java/com/tempotelecom/pedidosappback/PedidosAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.tempotelecom.pedidosappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosAppBackApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/tempotelecom/pedidosappback/PedidosAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempotelecom.pedidosappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
